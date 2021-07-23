64d8128d3971d7062655d938e403936c1eb675e4
jpramirezlo <jramirez2803@gmail.com>
2021-07-23 18:50:07 -0500

    Se mejora el codigo, donde se agrega etiqueta @Before y @After

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,3 @@
+
+H
+PruebaAutomatizacion.iml,d\6\d61341152353fbf44476ca6f19e863ef54c4b322
--- src/test/java/PruebaTest.java
+++ src/test/java/PruebaTest.java
@@ -1,5 +1,11 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -9,24 +15,57 @@ public class PruebaTest {
 
   protected WebDriver chromeDriver;
 
-  @Test
-  public void hacer_una_busqueda() {
-    //Encontrar archivo exe de chromedriver
-    System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+  @Before
+  public void abrirDriver() {
 
-    //Creamos opciones sobre nuestro driver
+    // Encontrar archivo exe de chromedriver
+    System.setProperty("webdriver.chrome.driver", "src\\main\\resources\\chromedriver.exe");
+
+    // Creamos opciones sobre nuestro driver
     ChromeOptions options = new ChromeOptions();
     options.setCapability("marionette", false);
 
-    //Nueva instancia de ChromeDriver
+    // Nueva instancia de ChromeDriver
     chromeDriver = new ChromeDriver(options);
 
-    //Definimos tiempo de espera hasta que aceptemos un timeOut
+    // Definimos tiempo de espera hasta que aceptemos un timeOut
     chromeDriver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+  }
 
-    //Abrimos una URL
+  @Test
+  public void hacer_una_busqueda() {
+
+    // Abrimos una URL
     chromeDriver.get("https://www.google.com");
 
-    //Encontrar e interactuar con elementos
+    // Encontrar e interactuar con elementos
+
+    // Ingresar texto en el buscador
+    chromeDriver
+        .findElement(By.cssSelector("input[name='q']"))
+        .sendKeys("Cantidad de paises en America");
+
+    // Presionamos la tecla Enter
+    chromeDriver.findElement(By.cssSelector("input[name='q']")).sendKeys(Keys.ENTER);
+
+    // Hacer click sobre el link deseado
+    chromeDriver
+        .findElement(
+            By.cssSelector("#rso > div:nth-child(11) > div > div.tF2Cxc > div.yuRUbf > a > h3"))
+        .click();
+
+    // Creamos un elemento web para poder hacer acciones avanzadas
+    WebElement subtitulo =
+        chromeDriver.findElement(By.xpath("//*[@id=\"post-7002\"]/div/p[2]/span[1]/strong"));
+
+    // Verificar que la cantidad de paises en America sigan siendo 35
+    Assert.assertTrue(subtitulo.getText().contains("35"));
+  }
+
+  @After
+  public void cerrarDriver() {
+
+    // Cerrar ventana, apagar driver
+    chromeDriver.quit();
   }
 }
